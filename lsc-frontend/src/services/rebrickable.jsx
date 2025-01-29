d8c4a7a3d55d5c04b1d7576168db5449d7c7e092
rekamakadi <r.makadi@gmail.com>
2025-01-29 10:56:26 +0100

    [fix] .env key name

--- lsc-frontend/src/services/rebrickable.jsx
+++ lsc-frontend/src/services/rebrickable.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = import.meta.env.REBRICKABLE_API_KEY;
+const API_KEY = import.meta.env.VITE_REBRICKABLE_API_KEY;
 const BASE_URL = 'https://rebrickable.com/api/v3/lego/';
 
 const apiClient = axios.create({
