82c7fa29ed6d7d6235f22e8fa007195b15d6c22c
rekamakadi <r.makadi@gmail.com>
2025-02-03 01:38:13 +0100

    [feat] add GlowEffect

--- lsc-frontend/src/components/PartListItem.jsx
+++ lsc-frontend/src/components/PartListItem.jsx
@@ -1,31 +1,51 @@
 import { Card, CardContent, CardMedia, TextField, Typography, Box } from "@mui/material";
+import GlowEffect from "./GlowEffect";
 
 function PartListItem({ part, foundCount, updatePartCount }) {
+  const isComplete = foundCount === part.quantity;
+
   return (
-    <Card sx={{ display: "flex", flexDirection: "column", alignItems: "center", padding: 1, width: "100%", height: "100%", minWidth: 100, maxWidth: 150, m: 1 }}>
-      <CardMedia
-        component="img"
-        image={part.part.part_img_url}
-        alt={part.part.name}
-        sx={{ width: 80, height: 80, objectFit: "contain" }}
-      />
-      <CardContent sx={{ textAlign: "center", width: "100%", flexGrow: 1 }}>
-        <Typography variant="body1">Part Num: {part.part.part_num}</Typography>
-        <Typography variant="body2">Part ID: {part.id}</Typography>
-        <Typography variant="body2">Required: {part.quantity}</Typography>
-        <Box sx={{ mt: 1, width: "100%" }}>
-          <TextField
-            type="number"
-            value={foundCount}
-            onChange={(e) => updatePartCount(part.id, Number(e.target.value), part.quantity)}
-            slotProps={{ input: { min: 0, max: part.quantity } }}
-            sx={{ width: "50%" }}
-            label="Found"
-            variant="outlined"
+    <Box sx={{ position: "relative", display: "inline-block" }}>
+      <GlowEffect isComplete={isComplete}>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: 1,
+            width: "100%",
+            height: "100%",
+            minWidth: 100,
+            maxWidth: 150,
+            position: "relative",
+            zIndex: 1,
+          }}
+        >
+          <CardMedia
+            component="img"
+            image={part.part.part_img_url}
+            alt={part.part.name}
+            sx={{ width: 80, height: 80, objectFit: "contain" }}
           />
-        </Box>
-      </CardContent>
-    </Card>
+          <CardContent sx={{ textAlign: "center", width: "100%", flexGrow: 1 }}>
+            <Typography variant="body1">Part Num: {part.part.part_num}</Typography>
+            <Typography variant="body2">Part ID: {part.id}</Typography>
+            <Typography variant="body2">Required: {part.quantity}</Typography>
+            <Box sx={{ mt: 1, width: "100%" }}>
+              <TextField
+                type="number"
+                value={foundCount}
+                onChange={(e) => updatePartCount(part.id, Number(e.target.value), part.quantity)}
+                slotProps={{ input: { min: 0, max: part.quantity } }}
+                sx={{ width: "50%" }}
+                label="Found"
+                variant="outlined"
+              />
+            </Box>
+          </CardContent>
+        </Card>
+      </GlowEffect>
+    </Box>
   );
 }
 
